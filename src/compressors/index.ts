6f80a41309e3b12f7a81a00fc5786f7cb4c7a117
Toby Nguyen <tobynguyen27@proton.me>
2026-06-21 15:35:43 +0700

    chore: improve log

--- src/compressors/index.ts
+++ src/compressors/index.ts
@@ -1,38 +1,63 @@
 import { getFileExtension } from '@/utils/file';
-import { forEach, gen } from 'effect/Effect';
+import { forEach, gen, timed } from 'effect/Effect';
 import type { OutputAsset, OutputBundle } from 'rolldown';
 import { pngCompressor } from './png';
 import { jpegCompressor } from './jpeg';
 import { webpCompressor } from './webp';
 import { avifCompressor } from './avif';
-import type { Options } from '@/options';
+import { Config } from '@/options';
+import { Logger } from '@/logger';
+import { Duration, pipe, Ref } from 'effect';
+import prettyBytes from 'pretty-bytes';
 
-export const compressor = (bundles: OutputBundle, options: Options) =>
-	forEach(
-		Object.values(bundles).filter(
+export const compressor = (bundles: OutputBundle) =>
+	gen(function* () {
+		const bundlesNeedCompress = Object.values(bundles).filter(
 			(bundle): bundle is OutputAsset & { source: Uint8Array } =>
 				bundle.type === 'asset' && typeof bundle.source !== 'string',
-		),
-		(bundle) =>
-			gen(function* () {
-				const fileExtension = getFileExtension(bundle.fileName);
-				const buffer = bundle.source;
+		);
 
-				switch (fileExtension) {
-					case 'png':
-						bundle.source = yield* pngCompressor(buffer);
-						break;
-					case 'jpg':
-					case 'jpeg':
-						bundle.source = yield* jpegCompressor(buffer);
-						break;
-					case 'webp':
-						bundle.source = yield* webpCompressor(buffer);
-						break;
-					case 'avif':
-						bundle.source = yield* avifCompressor(buffer);
-						break;
-				}
-			}),
-		{ concurrency: options.concurrency },
-	);
+		const { concurrency } = yield* Config;
+		const counter = yield* Ref.make(1);
+
+		yield* forEach(
+			bundlesNeedCompress,
+			(bundle) =>
+				gen(function* () {
+					const logger = yield* Logger;
+
+					const fileExtension = getFileExtension(bundle.fileName);
+					const buffer = bundle.source;
+
+					const oldSizeInBytes = prettyBytes(buffer.byteLength);
+					const [duration, newBuffer] = yield* pipe(
+						gen(function* () {
+							switch (fileExtension) {
+								case 'png':
+									return yield* pngCompressor(buffer);
+								case 'jpg':
+								case 'jpeg':
+									return yield* jpegCompressor(buffer);
+								case 'webp':
+									return yield* webpCompressor(buffer);
+								case 'avif':
+									return yield* avifCompressor(buffer);
+								default:
+									return buffer;
+							}
+						}),
+						timed,
+					);
+					const resolveTimeMs = pipe(Duration.toMillis(duration), Math.round);
+
+					bundle.source = newBuffer;
+
+					const index = yield* Ref.getAndUpdate(counter, (n) => n + 1);
+
+					logger.info(
+						`${bundle.fileName} (before: ${oldSizeInBytes}) (after: ${prettyBytes(newBuffer.byteLength)}) (${resolveTimeMs}ms) (${index}/${Object.values(bundles).length - 1})`,
+					);
+				}),
+			{ concurrency },
+		);
+	});
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { Duration, Layer, pipe } from 'effect';
 import { compressor } from './compressors';
 import { convertMillisecondsToSeconds } from './utils/time';
 import pc from 'picocolors';
+import { Logger } from './logger';
 
 export default function Opmage(opts: Partial<Options> = {}): Plugin {
 	const options = { ...defaultOptions, ...opts } satisfies Options;
@@ -14,9 +15,16 @@ export default function Opmage(opts: Partial<Options> = {}): Plugin {
 	return {
 		name: 'rolldown-plugin-opmage',
 		async generateBundle(_, bundles) {
+			const LoggerLive = Layer.succeed(Logger, {
+				info: this.info,
+				warn: this.warn,
+				error: this.error,
+			});
+
 			const program = pipe(
-				compressor(bundles, options),
+				compressor(bundles),
 				provide(ConfigLive),
+				provide(LoggerLive),
 
 				timed,
 				tap(([duration]) => {
